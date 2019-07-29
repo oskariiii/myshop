c8b66e09fe0d82f582a0304461b4bb6f064c269b
frankie <frankie@bengder.cc>
2019-07-29 19:15:33 +0800

    修改User 模型域名空间,å路由添加中间件

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/','PagesController@root')->name('root');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/','PagesController@root')->name('root')->middleware('verified');;
+Auth::routes(['verify' => true]);
